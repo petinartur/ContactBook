c49eb71d6e504b5e2bab489a962417cc96571a54
Artur <petin.art@gmail.com>
2022-04-27 16:41:23 +0300

    Сделал секции

--- ContactBook/PersonListViewController.swift
+++ ContactBook/PersonListViewController.swift
@@ -9,31 +9,34 @@ import UIKit
 
 class PersonListViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.rowHeight = 80
-    }
-    
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         personList.count
     }
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        personList[section].fullName
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "contactList", for: indexPath)
-        let person = personList[indexPath.row]
         
+        let person = personList[indexPath.section]
         var content = cell.defaultContentConfiguration()
-        
         
         content.text = person.mail
         content.secondaryText = person.phonenumber
+                
         cell.contentConfiguration = content
         
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "header \(section)"
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
+    
 }
